87aedf6eb227b2535e18d71c7612a025a16467e3
Sly Gryphon <sgryphon@computer.org>
2010-08-17 19:04:09 +1000

    API documentation comments

--- Essential.Diagnostics/Diagnostics/ExpressionFilter.cs
+++ Essential.Diagnostics/Diagnostics/ExpressionFilter.cs
@@ -6,22 +6,45 @@ using System.Text;
 
 namespace Essential.Diagnostics
 {
+    /// <summary>
+    /// Trace filter that evaluates a user-supplied boolean expression.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The initalizeData for the filter contains the C# expression that
+    /// is evaluated. The expression must return a boolean result and
+    /// can use any of the parameters: Source, EventType, Id, Format.
+    /// </para>
+    /// <para>
+    /// Note that Format is the format template or message (if there are no
+    /// format parameters).
+    /// </para>
+    /// </remarks>
     public class ExpressionFilter : TraceFilter
     {
         Func<string, TraceEventType, int, string, bool> compiledExpression;
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="expression">A C# expression that is evaluated to determine if the event should be traced or not.</param>
         public ExpressionFilter(string expression)
         {
             if (expression == null) throw new ArgumentNullException("expression");
 
-            // , "LogicalOperationStack", "DateTime", "ThreadName", "ProcessName", "MachineName"
+            // TODO: Other properties - "LogicalOperationStack", "DateTime", "ThreadName", "ProcessName", "MachineName"
             // Note: Func<> has max 4 parameters; need one to be "Event", e.g. "Event.Id", etc??
-            // Note: "Format" may be the entire message
+            // ... but not sure how the compiled code can get a reference to the "Event" object (class).
+            // May have to give up on the expression idea and build a parser that directly calls
+            // a function.
             var parser = new ExpressionParser<Func<string, TraceEventType, int, string, bool>>("Source", "EventType", "Id", "Format");
 
             compiledExpression = parser.Parse(expression).Compile();
         }
 
+        /// <summary>
+        /// Determines whether the event should be traced by the listener or not.
+        /// </summary>
         public override bool ShouldTrace(TraceEventCache cache, string source, TraceEventType eventType, int id, string formatOrMessage, object[] args, object data1, object[] data)
         {
             return compiledExpression(source, eventType, id, formatOrMessage);
--- Essential.Diagnostics/Diagnostics/LogicalOperationScope.cs
+++ Essential.Diagnostics/Diagnostics/LogicalOperationScope.cs
@@ -6,24 +6,54 @@ using System.Diagnostics;
 
 namespace Essential.Diagnostics
 {
+    /// <summary>
+    /// Encompases a logical operation using the diagnostics correlation manager,
+    /// starting the operation when the object is created and ending the
+    /// operation when the object is disposed.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The scope can optionally log start and stop trace events for
+    /// the operation.
+    /// </para>
+    /// </remarks>
     public class LogicalOperationScope : IDisposable
     {
         TraceSource _source;
         int _startId;
         int _stopId;
 
+        /// <summary>
+        /// Constructor. 
+        /// Encompases an unnamed logical operation.
+        /// </summary>
         public LogicalOperationScope() : this(null, null, 0, 0)
         {
         }
 
+        /// <summary>
+        /// Constructor. 
+        /// Encompases a logical operation using the specified object.
+        /// </summary>
         public LogicalOperationScope(object operationId) : this(null, operationId, 0, 0)
         {
         }
 
+        /// <summary>
+        /// Constructor. 
+        /// Encompass a logical operation using the specified object, 
+        /// and writing start and stop events to the specified source.
+        /// </summary>
         public LogicalOperationScope(TraceSource source, object operationId) : this(source ,operationId, 0, 0)
         {
         }
 
+        /// <summary>
+        /// Constructor. 
+        /// Encompases a logical operation using the specified object,
+        /// and writing start and stop events to the specified source,
+        /// with the specified event IDs.
+        /// </summary>
         public LogicalOperationScope(TraceSource source, object operationId, int startId, int stopId)
         {
             _source = source;
@@ -47,12 +77,20 @@ namespace Essential.Diagnostics
             }
         }
 
+        /// <summary>
+        /// Dispose.
+        /// Ends the logical operation.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Dispose.
+        /// Ends the logical operation.
+        /// </summary>
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
