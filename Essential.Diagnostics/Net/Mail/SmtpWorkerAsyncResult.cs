886622d4c856bdfc29d73adb83d772e7d7a46618
sgryphon@computer.org <sgryphon@computer.org>
2013-04-17 10:31:19 +1000

    Combine the functions of MailMessageQueue and SmtpClientPool into one SmtpWorkerPool class.

---
+++ Essential.Diagnostics/Net/Mail/SmtpWorkerAsyncResult.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+
+namespace Essential.Net.Mail
+{
+    // See
+    // http://msdn.microsoft.com/en-us/magazine/cc163467.aspx
+    // http://blogs.msdn.com/b/nikos/archive/2011/03/14/how-to-implement-iasyncresult-in-another-way.aspx
+
+    internal class SmtpWorkerAsyncResult : IAsyncResult, IDisposable
+    {
+        private const int c_StatePending = 0;
+        private const int c_StateCompletedSynchronously = 1;
+        private const int c_StateCompletedAsynchronously = 2;
+
+        private readonly AsyncCallback m_AsyncCallback; 
+        private readonly Object m_AsyncState;
+        private MailMessage message;
+
+        // Fields set at construction which do change after 
+        // operation completes 
+        private int m_CompletedState = c_StatePending;
+
+        // Field that may or may not get set depending on usage 
+        private ManualResetEvent m_AsyncWaitHandle;
+
+        // Fields set when operation completes 
+        private Exception m_exception;
+
+        internal SmtpWorkerAsyncResult(MailMessage message, AsyncCallback asyncCallback, object state)
+        {
+            this.message = message;
+            this.m_AsyncCallback = asyncCallback; 
+            this.m_AsyncState = state;
+        }
+
+        public MailMessage Message { get { return message; } }
+
+        public Object AsyncState { 
+            get { return m_AsyncState; } 
+        }
+
+        public WaitHandle AsyncWaitHandle
+        {
+            get
+            {
+                if (m_AsyncWaitHandle == null)
+                {
+                    Boolean done = IsCompleted; ManualResetEvent mre = new ManualResetEvent(done);
+                    if (Interlocked.CompareExchange(ref m_AsyncWaitHandle, mre, null) != null)
+                    {
+                        // Another thread created this object's event; dispose 
+                        // the event we just created
+                        mre.Close();
+                    }
+                    else
+                    {
+                        if (!done && IsCompleted)
+                        {
+                            // If the operation wasn't done when we created 
+                            // the event but now it is done, set the event
+                            m_AsyncWaitHandle.Set();
+                        }
+                    }
+                }
+                return m_AsyncWaitHandle;
+            }
+        }
+
+        public Boolean CompletedSynchronously
+        {
+            get
+            {
+                return Thread.VolatileRead(ref m_CompletedState) == c_StateCompletedSynchronously;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public Boolean IsCompleted
+        {
+            get
+            {
+                return Thread.VolatileRead(ref m_CompletedState) != c_StatePending;
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Although has message field, does not own it -- it was passed into constructor.
+                // (i.e. not responsible for disposing of it, but do want to release reference).
+                message = null;
+                //if (message != null)
+                //{
+                //    message.Dispose();
+                //}
+
+                if (m_AsyncWaitHandle != null)
+                {
+                    ((IDisposable)m_AsyncWaitHandle).Dispose();
+                }
+            }
+        }
+
+        internal void Complete(Exception exception, bool completedSynchronously)
+        {
+            // The m_CompletedState field MUST be set prior calling the callback 
+            int prevState = Interlocked.Exchange(ref m_CompletedState,
+                completedSynchronously ? c_StateCompletedSynchronously : c_StateCompletedAsynchronously);
+
+            if (prevState == c_StatePending)
+            {
+                // Passing null for exception means no error occurred. 
+                // This is the common case 
+                m_exception = exception;
+
+                // If the event exists, set it 
+                if (m_AsyncWaitHandle != null)
+                {
+                    m_AsyncWaitHandle.Set();
+                }
+
+                // If a callback method was set, call it 
+                if (m_AsyncCallback != null)
+                {
+                    m_AsyncCallback(this);
+                }
+            }
+        }
+
+        internal static void End(SmtpWorkerAsyncResult asyncResult)
+        {
+            // This method assumes that only 1 thread calls EndInvoke 
+            // for this object 
+            if (!asyncResult.IsCompleted)
+            {
+                // If the operation isn't done, wait for it
+                asyncResult.AsyncWaitHandle.WaitOne();
+                asyncResult.AsyncWaitHandle.Close();
+                // Allow early GC 
+                asyncResult.m_AsyncWaitHandle = null;
+            }
+
+            // Operation is done: if an exception occured, throw it
+            if (asyncResult.m_exception != null)
+            {
+                throw asyncResult.m_exception;
+            }
+        }
+
+
+    }
+}
---
+++ Essential.Diagnostics/Net/Mail/SmtpWorkerPool.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+
+namespace Essential.Net.Mail
+{
+    // This class is needed because although SmtpClient has a SendAsync() method, it also states:
+    // "After calling SendAsync, you must wait for the e-mail transmission to complete before attempting 
+    // to send another e-mail message using Send or SendAsync."
+
+    // That means any following message must block until the SmtpClient is available, but of course
+    // there isn't really any status flag or anything, so you would need to implement that all
+    // yourself (meanwhile blocking the calling thread).
+
+    // So, to avoid blocking the calling thread there are two possible options.
+    // One is to spin off a sending thread for each message that blocks until it can proceed;
+    // threads are expensive, so the ThreadPool may be an option (to queue work up),
+    // but generally you have less control over the ThreadPool (it is shared with the system).
+  
+    // The other alternative is to have a queue in memory where the calling thread adds to the
+    // queue, and then have a background thread (or several) that keeps polling and dequeues messages 
+    // when available and sends them. This may give a bit better visibility to the size of the queue
+    // (rather than hidden off as blocked threads or ThreadPool queued work items). e.g. if the size
+    // of the queue is too large it can start "dropping packets", and simply not send messages when
+    // flooded.
+
+    // Note that the blocking threads / ThreadPool option pretty much works the same (i.e. the messages
+    // are queued up somewhere in memory), just with less control.
+
+    // Also note that in both cases you almost might as well send the email message synchronously, as
+    // you need to wait for it to complete anyway.
+
+    internal class SmtpWorkerPool : IDisposable
+    {
+        int maxConnections;
+        Queue<SmtpWorkerAsyncResult> messageQueue = new Queue<SmtpWorkerAsyncResult>();
+        object queueLock = new object();
+        //Stack<SmtpWorker> smtpWorkerPool = new Stack<SmtpWorker>();
+        SmtpWorker smtpWorker;
+
+        public SmtpWorkerPool(int maxConnections)
+        {
+            this.maxConnections = maxConnections;
+        }
+
+        public IAsyncResult BeginSend(MailMessage message, AsyncCallback callback, object state)
+        {
+            var asyncResult = new SmtpWorkerAsyncResult(message, callback, state);
+            lock (queueLock)
+            {
+                messageQueue.Enqueue(asyncResult);
+                EnsureWorker();
+            }
+            return asyncResult;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void EndSend(IAsyncResult asyncResult)
+        {
+            var smtpResult = asyncResult as SmtpWorkerAsyncResult;
+            if (smtpResult == null)
+            {
+                throw new ArgumentException("IAsyncResult is of the wrong type.", "asyncResult");
+            }
+            SmtpWorkerAsyncResult.End(smtpResult);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                var worker = smtpWorker;
+                if (worker != null)
+                {
+                    worker.Dispose();
+                }
+                while (messageQueue.Count > 0)
+                {
+                    var asyncResult = messageQueue.Dequeue();
+                    asyncResult.Dispose();
+                }
+            }
+        }
+
+        private void EnsureWorker()
+        {
+            //if (smtpWorkerPool.Count == 0)
+            //{
+            //    var worker = new SmtpWorker(this);
+            //}
+            if (smtpWorker == null)
+            {
+                smtpWorker = new SmtpWorker(this);
+                smtpWorker.Start();
+            }
+        }
+
+        private class SmtpWorker : IDisposable
+        {
+            SmtpWorkerPool pool;
+            SmtpClient SmtpClient { get; set; }
+            Thread Thread { get; set; }
+
+            public SmtpWorker(SmtpWorkerPool pool)
+            {
+                this.pool = pool;
+                Thread = new Thread(ThreadStart);
+                Thread.IsBackground = true;
+                Thread.Name = "SmtpWorker";
+            }
+
+            public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            public void Start()
+            {
+                Thread.Start();
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (disposing)
+                {
+                    if (SmtpClient != null)
+                    {
+                        if (typeof(SmtpClient).GetInterface("IDisposable") != null)//In .NET 4, SmtpClient has IDisposable.
+                        {
+                            SmtpClient client = SmtpClient;
+                            IDisposable disposable = client as IDisposable;
+                            disposable.Dispose();
+                        }
+                    }
+                }
+            }
+
+            private void ThreadStart()
+            {
+                SmtpClient = new SmtpClient();
+
+                while (true)
+                {
+                    SmtpWorkerAsyncResult asyncResultToProcess = null;
+                    lock (pool.queueLock)
+                    {
+                        if (pool.messageQueue.Count > 0)
+                        {
+                            asyncResultToProcess = pool.messageQueue.Dequeue();
+                        }
+                    }
+                    if (asyncResultToProcess != null)
+                    {
+                        var message = asyncResultToProcess.Message;
+                        Exception exception = null;
+                        try
+                        {
+                            SmtpClient.Send(message);
+                        }
+                        catch (Exception ex)
+                        {
+                            exception = ex;
+                        }
+                        asyncResultToProcess.Complete(exception, false);
+                    }
+                    Thread.Sleep(0);
+                }
+
+            }
+        }
+    }
+}
