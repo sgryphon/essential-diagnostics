92c47c6a279824c7996c654b833e79e32ad52ea8
Sly Gryphon <sgryphon@computer.org>
2016-03-19 08:52:53 +1000

    Add logical operation scope message support, similar to activity scope.

--- Tests/Essential.Diagnostics.Tests/LogicalOperationScopeTests.cs
+++ Tests/Essential.Diagnostics.Tests/LogicalOperationScopeTests.cs
@@ -43,5 +43,72 @@ namespace Essential.Diagnostics.Tests
             var logicalOperationStack2 = events[2].GetLogicalOperationStack();
             Assert.AreEqual(0, logicalOperationStack2.Length);
         }
+
+        [TestMethod]
+        public void ScopeDefaultMessages()
+        {
+            TraceSource source = new TraceSource("inmemory1Source");
+            var listener = source.Listeners.OfType<InMemoryTraceListener>().First();
+            listener.Clear();
+
+            using (var scope1 = new LogicalOperationScope(source, "X"))
+            {
+                source.TraceEvent(TraceEventType.Warning, 1, "A");
+            }
+
+            var events = listener.GetEvents();
+
+            Assert.AreEqual(3, events.Length);
+            Assert.AreEqual("Start operation", events[0].Message);
+            Assert.AreEqual("Stop operation", events[2].Message);
+        }
+
+        [TestMethod]
+        public void ScopeCustomMessages()
+        {
+            TraceSource source = new TraceSource("inmemory1Source");
+            var listener = source.Listeners.OfType<InMemoryTraceListener>().First();
+            listener.Clear();
+
+            using (var scope1 = new LogicalOperationScope(source, "X", 11, 12, "Operation Go", "Done"))
+            {
+                source.TraceEvent(TraceEventType.Warning, 1, "A");
+            }
+
+            var events = listener.GetEvents();
+
+            Assert.AreEqual(3, events.Length);
+            Assert.AreEqual("Operation Go", events[0].Message);
+            Assert.AreEqual(11, events[0].Id);
+            Assert.AreEqual("Done", events[2].Message);
+            Assert.AreEqual(12, events[2].Id);
+        }
+
+        [TestMethod]
+        public void ScopeManualStartStopMessages()
+        {
+            TraceSource source = new TraceSource("inmemory1Source");
+            var listener = source.Listeners.OfType<InMemoryTraceListener>().First();
+            listener.Clear();
+
+            var operationId = "X";
+            using (var scope1 = new LogicalOperationScope(operationId))
+            {
+                source.TraceEvent(TraceEventType.Start, 11, "Start {0}", operationId);
+
+                source.TraceEvent(TraceEventType.Warning, 1, "A");
+
+                source.TraceEvent(TraceEventType.Stop, 12, "Stop {0}", operationId);
+            }
+
+            var events = listener.GetEvents();
+
+            Assert.AreEqual(3, events.Length);
+            Assert.AreEqual("Start X", events[0].Message);
+            Assert.AreEqual(11, events[0].Id);
+            Assert.AreEqual("Stop X", events[2].Message);
+            Assert.AreEqual(12, events[2].Id);
+        }
+
     }
 }
