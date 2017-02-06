bdd57531a8173dc1d9ecad12539cc0248b03aa57
Sly Gryphon <sgryphon@computer.org>
2017-02-07 07:33:46 +1000

    Net Core examples

---
+++ Examples/TestNetCore/TestEventSource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics.Tracing;
+
+namespace HelloNetCore
+{
+    public class TestEventSource
+    {
+        public void Run()
+        {
+            Console.WriteLine("EventSource Example (.NET Core 1.0)");
+
+            var customEventSource = new CustomEventSource();
+            customEventSource.Information("Test .NET Core 1.0");
+        }
+
+        [EventSource(Guid = "{8D123D8D-BABC-4A9D-A506-512510EEC510}", Name = "CustomEventSource")]
+        public class CustomEventSource : EventSource
+        {
+            [Event(1,
+                Level = EventLevel.Informational,
+                //Message = "Information: {0}",
+                Keywords = EventKeywords.None,
+                Opcode = EventOpcode.Info,
+                Task = EventTask.None)]
+            public void Information(string message)
+            {
+                this.WriteEvent(2, message);
+            }
+        }
+    }
+}
--- Examples/TestNetCore/TestTraceSource.cs
+++ Examples/TestNetCore/TestTraceSource.cs
@@ -6,22 +6,21 @@ using System.Threading.Tasks;
 
 namespace HelloNetCore
 {
-    public class Program
+    public class TestTraceSource
     {
-        static TraceSource trace = new TraceSource("Example.ColoredConsole.Source", SourceLevels.All);
 
-        public static void Main(string[] args)
+        public void Run()
         {
-            Console.WriteLine("Colored Console Example (.NET Core 1.0)");
+            Console.WriteLine("TraceSource Example (.NET Core 1.0)");
 
+            // There is no config support for TraceSource, so need to add listener manually
+            var trace = new TraceSource("Example.ColoredConsole.Source", SourceLevels.All);
             var consoleListener = new TextWriterTraceListener(Console.Out);
             trace.Listeners.Add(consoleListener);
 
             trace.TraceEvent(TraceEventType.Information, 1001, "Net20 example started.");
             trace.TraceEvent(TraceEventType.Verbose, 0, "Net20 verbose.");
             trace.TraceEvent(TraceEventType.Warning, 4001, "Net20 warning.");
-
-            Console.ReadLine();
         }
     }
 }
