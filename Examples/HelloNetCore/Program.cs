6fed3a19e82350ef633980142117a996b3721964
Sly Gryphon <sgryphon@computer.org>
2017-02-06 21:42:54 +1000

    .NET Core example skeleton

---
+++ Examples/HelloNetCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Colored Console Example (.NET Core 1.0)");
+            Console.ReadLine();
+        }
+    }
+}
