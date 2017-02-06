5a6000e6b0f97744b4351c3eca44affc81d42e57
Sly Gryphon <sgryphon@computer.org>
2017-02-06 21:50:36 +1000

    Add tracesource to core 1.0 example

--- Examples/HelloNetCore/Program.cs
+++ Examples/HelloNetCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,14 @@ namespace HelloNetCore
 {
     public class Program
     {
+        static TraceSource trace = new TraceSource("Example.ColoredConsole.Source");
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Colored Console Example (.NET Core 1.0)");
+            trace.TraceEvent(TraceEventType.Information, 1001, "Net20 example started.");
+            trace.TraceEvent(TraceEventType.Verbose, 0, "Net20 verbose.");
+            trace.TraceEvent(TraceEventType.Warning, 4001, "Net20 warning.");
             Console.ReadLine();
         }
     }
