17d754cede16df2ca69e5e51a2a1546e813c11b2
Sly Gryphon <sgryphon@computer.org>
2017-02-06 22:27:44 +1000

    Manually add listener to trace source (no config)

--- Examples/HelloNetCore/Program.cs
+++ Examples/HelloNetCore/Program.cs
@@ -8,14 +8,19 @@ namespace HelloNetCore
 {
     public class Program
     {
-        static TraceSource trace = new TraceSource("Example.ColoredConsole.Source");
+        static TraceSource trace = new TraceSource("Example.ColoredConsole.Source", SourceLevels.All);
 
         public static void Main(string[] args)
         {
             Console.WriteLine("Colored Console Example (.NET Core 1.0)");
+
+            var consoleListener = new TextWriterTraceListener(Console.Out);
+            trace.Listeners.Add(consoleListener);
+
             trace.TraceEvent(TraceEventType.Information, 1001, "Net20 example started.");
             trace.TraceEvent(TraceEventType.Verbose, 0, "Net20 verbose.");
             trace.TraceEvent(TraceEventType.Warning, 4001, "Net20 warning.");
+
             Console.ReadLine();
         }
     }
