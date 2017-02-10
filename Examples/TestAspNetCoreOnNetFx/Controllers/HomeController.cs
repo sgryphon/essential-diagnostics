54ae52b9d215e1c4ef09c95a0f4774bc7a8f86f8
Sly Gryphon <sgryphon@computer.org>
2017-02-10 14:56:53 +1000

    Working example of tracing

--- Examples/TestAspNetCoreOnNetFx/Controllers/HomeController.cs
+++ Examples/TestAspNetCoreOnNetFx/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 
 namespace TestAspNetCoreOnNetFx.Controllers
 {
     public class HomeController : Controller
     {
+        TraceSource traceSource = new TraceSource("TraceSource.TestAspNetCoreOnNetFx");
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +20,13 @@ namespace TestAspNetCoreOnNetFx.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+
+            traceSource.TraceEvent(TraceEventType.Information, 1001, "TraceSource from About controller: {0}", Guid.NewGuid());
+
+            var loggerFactory = new LoggerFactory();
+            loggerFactory.AddTraceSource(new SourceSwitch("sourceSwitch") { Level = SourceLevels.All });
+            var logger = loggerFactory.CreateLogger("Logger.TestAspNetCoreOnNetFx");
+            logger.LogInformation(1002, "Logger from About controller: {0}", Guid.NewGuid());
 
             return View();
         }
