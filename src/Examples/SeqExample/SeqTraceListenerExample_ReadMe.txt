7e47a8e85b63f0a0a55ab5b0a93a768fcbd0a729
Sly Gryphon <sgryphon@computer.org>
2017-07-17 21:37:00 +1000

    Structured tracing examples.

---
+++ src/Examples/SeqExample/SeqTraceListenerExample_ReadMe.txt
@@ -0,0 +1,27 @@
+﻿Essential.Diagnostics - Seq Trace Listener Example
+==================================================
+
+Examples using SeqTraceListener to send events to a Seq Server (https://getseq.net/).
+
+The message template and arguments are passed as structured data to the Seq Server, allowing
+the individually logged items to be processed and filtered. Note that standard tracing
+only supports standard string format messages and args; although the arg types are preserved
+and logged separately, they have anonymous names. Args are simply named 0, 1, 2, etc.
+
+However, within a particular logging context, there is only a limited range of values, so
+you can still filter on a particular value, e.g. if CustomerID is logged as item {1}, then you
+can search and filter on property 1.
+
+Standard properties such as EventId, EventType, and ActivityId are included in all traces,
+with any TraceOutputOptions that are configured sent as additional properties. The listener
+also supports an expanded additionalProperties attribute to include other key data,
+such as MachineName and User.
+
+Instructions
+------------
+
+1. Download and install Seq Server - the personal developer version is sufficient
+2. Build the application.
+3. Run the application from the command line.
+4. Examine the output in Seq.
+5. Expand trace messages to see the individual data elements that have been traced.
---
+++ src/Examples/StructuredTracingExample/Program.cs
@@ -0,0 +1,60 @@
+﻿using Essential.Diagnostics;
+using Essential.Diagnostics.Structured;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuredTracing.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trace = new TraceSource("StructuredTracing.Example");
+            trace.TraceData(TraceEventType.Verbose, 0, 
+                new StructuredData(new Dictionary<string, object>
+                {
+                    { "EmailAddress", "alice@example.org" },
+                    { "Delay", 123.45 }
+                }, "Email sent"));
+            trace.TraceData(TraceEventType.Information, 1001, 
+                new StructuredData("City {Place} has coordinates {@Location}", 
+                "Brisbane", new Location() { Latitude = -27.5, Longitude = 153.0 }, Guid.NewGuid()));
+
+            trace.TraceStructuredData(TraceEventType.Warning, 3001, new Dictionary<string, object>
+                {
+                    { "CurrentValue", 49 },
+                    { "Threshold", 50 }
+                }, "Reached {CurrentValue}");
+
+            trace.TraceStructuredData(TraceEventType.Error, 5001, new ApplicationException(), 
+                "There was an error processing {OrderId}", 12345);
+
+            var structuredTrace = new AssemblyStructuredTrace<StandardEventId, Program>();
+
+            structuredTrace.Information(StandardEventId.ConfigurationStart, 
+                "Configuration started: {Address}", new IPAddress(new byte[] { 192, 168, 1, 1 }));
+
+            structuredTrace.Critical(StandardEventId.SystemCriticalError, 
+                new ApplicationException(), "System error");
+
+            structuredTrace.Verbose("Item is between {Low} and {High}", 22, 24);
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+
+    public class Location
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
