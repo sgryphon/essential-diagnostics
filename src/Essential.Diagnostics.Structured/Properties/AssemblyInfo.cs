222c61c8926d67b627b7a8af82c6eabe2f0001c6
Sly Gryphon <sgryphon@computer.org>
2017-09-17 20:28:50 +1000

    Update assembly info with InformationalVersion, for GitVersion

--- src/Essential.Diagnostics.Structured/Properties/AssemblyInfo.cs
+++ src/Essential.Diagnostics.Structured/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
-[assembly: AssemblyInformationalVersion("2.2.0-unstable.6+Branch.develop.Sha.57af87434f4c41dbf0d4b71dbf629c11da431247")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0+local")]
